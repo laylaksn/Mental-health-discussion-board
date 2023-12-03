d9c90864e47d52285866cd698907e9428ca5e9bc
layla sadeghi namaghi <laylasadeghi@gmail.com>
2023-12-03 18:06:57 +0000

    fb

--- server.js
+++ server.js
@@ -7,22 +7,30 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Your Firebase configuration
-const firebaseConfig = {
-  // Your Firebase configuration here
-};
-
 // Firebase setup
 const admin = require('firebase-admin');
-admin.initializeApp({
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "mental-health-discussions.firebaseapp.com",
+  projectId: "mental-health-discussions",
+  storageBucket: "mental-health-discussions.appspot.com",
+  messagingSenderId: "346523184946",
+  appId: "1:346523184946:web:7f13bf9a7d3c5736f5f12b",
+  measurementId: "G-J1ZG3VR8RE"
+};
+
+// Initialize Firebase
+const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig),
-  databaseURL: 'https://your-firebase-project-id.firebaseio.com',
+  databaseURL: 'https://mental-health-discussions.firebaseio.com/', // Update with your database URL
 });
 
 // API endpoint to save discussions
 app.post('/api/discussions', async (req, res) => {
   try {
-    const discussionsRef = admin.database().ref('discussions');
+    const discussionsRef = firebaseApp.database().ref('discussions');
     await discussionsRef.push(req.body);
     res.status(201).json({ message: 'Discussion saved successfully.' });
   } catch (error) {
@@ -34,7 +42,7 @@ app.post('/api/discussions', async (req, res) => {
 // API endpoint to retrieve discussions
 app.get('/api/discussions', async (req, res) => {
   try {
-    const discussionsSnapshot = await admin.database().ref('discussions').once('value');
+    const discussionsSnapshot = await firebaseApp.database().ref('discussions').once('value');
     const discussions = discussionsSnapshot.val();
     res.status(200).json(discussions || {});
   } catch (error) {
