a8cc4a1eeb06b1b8422a7ee9f9fc804a1e6e930d
layla sadeghi namaghi <laylasadeghi@gmail.com>
2023-12-03 12:35:21 +0000

    store discussion

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function addDiscussion() {
+// Load discussions from local storage on page load
+document.addEventListener('DOMContentLoaded', function () {
+    loadDiscussions();
+  });
+  
+  function addDiscussion() {
     const discussionInput = document.getElementById('discussionInput');
     const discussionsContainer = document.getElementById('discussions');
   
@@ -6,10 +11,21 @@ function addDiscussion() {
       const currentDate = new Date();
       const formattedDate = currentDate.toLocaleString();
   
-      const discussion = document.createElement('div');
-      discussion.className = 'discussion';
-      discussion.innerHTML = `<p>${discussionInput.value}</p><p class="timestamp">Posted on ${formattedDate}</p><button onclick="addComment(this)">Comment</button><button onclick="addLike(this)">Like</button>`;
-      discussionsContainer.appendChild(discussion);
+      // Create a discussion object
+      const discussionObject = {
+        content: discussionInput.value,
+        timestamp: formattedDate,
+        comments: [],
+        likes: 0,
+      };
+  
+      // Save the discussion to local storage
+      saveDiscussion(discussionObject);
+  
+      // Add the discussion to the UI
+      addDiscussionToUI(discussionObject, discussionsContainer);
+  
+      // Clear the input field
       discussionInput.value = '';
     }
   }
@@ -20,10 +36,17 @@ function addDiscussion() {
       const currentDate = new Date();
       const formattedDate = currentDate.toLocaleString();
   
-      const comment = document.createElement('div');
-      comment.className = 'comment';
-      comment.innerHTML = `<p>${commentInput}</p><p class="timestamp">Commented on ${formattedDate}</p>`;
-      button.parentNode.insertBefore(comment, button);
+      // Create a comment object
+      const commentObject = {
+        content: commentInput,
+        timestamp: formattedDate,
+      };
+  
+      // Add the comment to the UI
+      addCommentToUI(commentObject, button.parentNode);
+  
+      // Update the discussion in local storage
+      updateDiscussion(button.parentNode, { comments: [commentObject] });
     }
   }
   
@@ -32,5 +55,65 @@ function addDiscussion() {
     likesContainer.className = 'likes-container';
     likesContainer.innerHTML = '<p>1 like</p>';
     button.parentNode.insertBefore(likesContainer, button);
+  
+    // Update the discussion in local storage
+    updateDiscussion(button.parentNode, { likes: 1 });
+  }
+  
+  // Function to save a discussion to local storage
+  function saveDiscussion(discussion) {
+    let discussions = JSON.parse(localStorage.getItem('discussions')) || [];
+    discussions.push(discussion);
+    localStorage.setItem('discussions', JSON.stringify(discussions));
+  }
+  
+  // Function to load discussions from local storage and display them on the UI
+  function loadDiscussions() {
+    const discussionsContainer = document.getElementById('discussions');
+    let discussions = JSON.parse(localStorage.getItem('discussions')) || [];
+    discussions.forEach((discussion) => {
+      addDiscussionToUI(discussion, discussionsContainer);
+    });
+  }
+  
+  // Function to update a discussion in local storage
+  function updateDiscussion(discussionElement, updates) {
+    const discussions = JSON.parse(localStorage.getItem('discussions')) || [];
+    const discussionIndex = discussions.findIndex(
+      (d) => d.timestamp === discussionElement.querySelector('.timestamp').innerText
+    );
+    if (discussionIndex !== -1) {
+      discussions[discussionIndex] = { ...discussions[discussionIndex], ...updates };
+      localStorage.setItem('discussions', JSON.stringify(discussions));
+    }
+  }
+  
+  // Function to add a discussion to the UI
+  function addDiscussionToUI(discussion, container) {
+    const discussionElement = document.createElement('div');
+    discussionElement.className = 'discussion';
+    discussionElement.innerHTML = `<p>${discussion.content}</p><p class="timestamp">Posted on ${discussion.timestamp}</p><button onclick="addComment(this)">Comment</button><button onclick="addLike(this)">Like</button>`;
+    container.appendChild(discussionElement);
+  
+    // Add comments to the UI
+    discussion.comments.forEach((comment) => {
+      addCommentToUI(comment, discussionElement);
+    });
+  
+    // Add likes to the UI
+    for (let i = 0; i < discussion.likes; i++) {
+      const likesContainer = document.createElement('div');
+      likesContainer.className = 'likes-container';
+      likesContainer.innerHTML = '<p>1 like</p>';
+      discussionElement.insertBefore(likesContainer, discussionElement.lastChild);
+    }
+  }
+  
+  // Function to add a comment to the UI
+  function addCommentToUI(comment, container) {
+    const commentElement = document.createElement('div');
+    commentElement.className = 'comment';
+    commentElement.innerHTML = `<p>${comment.content}</p><p class="timestamp">Commented on ${comment.timestamp}</p>`;
+    container.insertBefore(commentElement, container.lastChild);
   }
   
