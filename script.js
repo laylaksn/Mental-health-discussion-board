d006ded103a97065ec4082f71b9e0025d6230a77
layla sadeghi namaghi <laylasadeghi@gmail.com>
2023-12-03 12:31:13 +0000

    saving comments

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ function addDiscussion() {
     const discussionsContainer = document.getElementById('discussions');
   
     if (discussionInput.value.trim() !== '') {
+      const currentDate = new Date();
+      const formattedDate = currentDate.toLocaleString();
+  
       const discussion = document.createElement('div');
       discussion.className = 'discussion';
-      discussion.innerHTML = `<p>${discussionInput.value}</p><button onclick="addComment(this)">Comment</button><button onclick="addLike(this)">Like</button>`;
+      discussion.innerHTML = `<p>${discussionInput.value}</p><p class="timestamp">Posted on ${formattedDate}</p><button onclick="addComment(this)">Comment</button><button onclick="addLike(this)">Like</button>`;
       discussionsContainer.appendChild(discussion);
       discussionInput.value = '';
     }
@@ -14,9 +17,12 @@ function addDiscussion() {
   function addComment(button) {
     const commentInput = prompt('Add a comment:');
     if (commentInput !== null && commentInput.trim() !== '') {
+      const currentDate = new Date();
+      const formattedDate = currentDate.toLocaleString();
+  
       const comment = document.createElement('div');
       comment.className = 'comment';
-      comment.innerHTML = `<p>${commentInput}</p>`;
+      comment.innerHTML = `<p>${commentInput}</p><p class="timestamp">Commented on ${formattedDate}</p>`;
       button.parentNode.insertBefore(comment, button);
     }
   }
